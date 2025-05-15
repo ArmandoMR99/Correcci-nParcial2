d4d7d66738cb88a7de5906590a3ddb3a8f39471c
ArmandoMR99 <armandorua999@gmail.com>
2025-05-14 22:47:31 -0500

    Create Sequence.cs

---
+++ TDD/Sequence.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviourTree
+{
+    public class Sequence : Composite
+    {
+        public override bool Execute()
+        {
+            foreach (var child in children)
+            {
+                if (!child.Execute())
+                    return false;
+            }
+            return true;
+        }
+    }
+}
