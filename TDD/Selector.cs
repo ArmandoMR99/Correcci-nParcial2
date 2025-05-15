dd199f7c4a7671e1059b2a68ab7d2e5079531737
ArmandoMR99 <armandorua999@gmail.com>
2025-05-14 22:46:59 -0500

    Create Selector.cs

---
+++ TDD/Selector.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviourTree
+{
+    public class Selector : Composite
+    {
+        public override bool Execute()
+        {
+            foreach (var child in children)
+            {
+                if (child.Execute())
+                    return true;
+            }
+            return false;
+        }
+    }
+}
